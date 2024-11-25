3ab9329abbaa36b50e28892fad361621e810bba9
khaos80 <duvan08oficial@gmail.com>
2024-11-25 03:06:08 -0500

    cambios realizados en settings y urls

--- sistema_transmetro/settings.py
+++ sistema_transmetro/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'sistema_transmetro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
